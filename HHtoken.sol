152bdd0674315ddc662636826666826ea28ea25e
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2018-07-06 12:38:34 -0700

    Create HHtoken.sol

---
+++ HHtoken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.4;
+
+contract ERC20Token {
+
+  function totalSupply() constant returns (uint256 supply) {}
+  
+  function balanceOf(address _owner) constant returns (uint256 balance) {}
+  
+  
