6cee3b16309b1f6bf3cc26c1745afe60605c484f
Hsien-Hsiu <hsien-hsiu.liao@yandex.com>
2018-08-06 07:36:44 -0700

    Update HHtoken.sol

--- HHtoken.sol
+++ HHtoken.sol
@@ -6,11 +6,28 @@ contract ERC20Token {
 
   function totalSupply() constant returns (uint256 supply) {}
   
-  function balanceOf(address _owner) constant returns (uint256 balance) {}
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+  }
   
-  function transfer(address _to, uint256 _value) returns (bool success) {}
+  function transfer(address _to, uint256 _value) returns (bool success) {
+          if (balances[msg.sender] >= _value && _value > 0 {
+              balances[msg.sender] -= _value;
+              balances[_to] += _value;
+              Transfer(msg.sender, _to, _value);
+              return true;
+          } else {return flase; }
+  }
   
-  function transferFrom(address _from, address _to, uint256 _value) returns (bool successs) {}
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool successs) {
+      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+          balances[_to] += _value;
+          balances[_from] -= _value;
+          allowed[_from][msg.sender] -= _value;
+          Transfer(_from, _to, _value);
+          return true;
+      } else { return false; }
+  }
   
   function approve(address _spender, uint256 _value)returns (bool sucess) {}
   
